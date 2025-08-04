969d42ec1fc46784d29d4730a1bccb549d162d6b
samueleblanc <samueleblanc17@gmail.com>
2025-08-04 15:36:15 -0400

    update version. now should be fully functioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="knowledgematrix",
-    version="0.0.1",
+    version="0.1.0",
     description="Compute the knowledge matrices",
     author="Marco Armenta and Samuel Leblanc",
     maintainer="Samuel Leblanc",
